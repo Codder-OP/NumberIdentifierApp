ff9bee930ca948ed025d5d9722ffdaf4095777e5
OM PRAKASH <OM PRAKASH@DESKTOP-43KMTN6>
2023-03-30 11:01:42 +0530

    Initial commit

---
+++ NumberIdentifierApi/src/main/java/com/springboot/app/NumberIdentifierApi/NumberIdentifierApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.NumberIdentifierApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberIdentifierApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberIdentifierApiApplication.class, args);
+	}
+
+}
